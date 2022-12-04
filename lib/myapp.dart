749b08365550d6c910e481a48063094c5fedae4d
iseruuuuu <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>
2022-12-04 17:39:03 +0900

    add: add getX

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:money_records_app/screen/tab/tab_screen.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'money_records_app',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
       home: const TabScreen(),
     );
