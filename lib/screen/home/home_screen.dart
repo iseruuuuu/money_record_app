a50b91fb3a2992c0138a56bb954ac0be14f7bcc6
iseruuuuu <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>
2022-12-04 10:12:51 +0900

    add_UI: Create HomeScreen

---
+++ lib/screen/home/home_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:money_records_app/components/elevated_buttons.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFCF8EA),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFDF8643),
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3.5,
+              child: const ElevatedButtons(
+                backgroundColor: Colors.white,
+                textColor: Color(0xFFD18448),
+                title: '収支',
+                isTap: true,
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3.5,
+              child: const ElevatedButtons(
+                backgroundColor: Color(0xFFD18448),
+                textColor: Colors.white,
+                title: '収入',
+                isTap: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40, bottom: 2),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                labelText: '割引前の金額',
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                labelText: '購入金額',
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 2),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'カテゴリー',
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'メモ',
+              filled: true,
+              fillColor: Colors.white,
+              border: InputBorder.none,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 42, top: 2),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: '日付',
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 100,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xFFDF8643),
+                onPrimary: const Color(0xFFDF8643),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                '保存',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+          const Spacer(flex: 5),
+        ],
+      ),
+    );
+  }
+}
