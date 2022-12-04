ef6d1d583e9bd467d4655eafdce27b3fe9d80ccc
iseruuuuu <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>
2022-12-04 10:26:08 +0900

    add: create textField close functions

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -36,90 +36,98 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40, bottom: 2),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                labelText: '割引前の金額',
-                filled: true,
-                fillColor: Colors.white,
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                labelText: '購入金額',
-                filled: true,
-                fillColor: Colors.white,
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 2),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'カテゴリー',
-                filled: true,
-                fillColor: Colors.white,
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: 'メモ',
-              filled: true,
-              fillColor: Colors.white,
-              border: InputBorder.none,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(bottom: 42, top: 2),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: '日付',
-                filled: true,
-                fillColor: Colors.white,
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width - 100,
-            height: 50,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: const Color(0xFFDF8643),
-                onPrimary: const Color(0xFFDF8643),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, bottom: 2),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(
+                      labelText: '割引前の金額',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                    ),
                   ),
                 ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                '保存',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(
+                      labelText: '購入金額',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                    ),
+                  ),
                 ),
-              ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'カテゴリー',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'メモ',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 42, top: 2),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: '日付',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 100,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFFDF8643),
+                      onPrimary: const Color(0xFFDF8643),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      '保存',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          const Spacer(flex: 5),
-        ],
+        ),
       ),
     );
   }
