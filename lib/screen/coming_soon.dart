5e76af25afdf70a8efcc30df5d9fe9baaa9a8b3a
iseruuuuu <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>
2022-12-18 16:25:48 +0900

    add: add coming_soon.dart

---
+++ lib/screen/coming_soon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ComingSoon extends StatelessWidget {
+  const ComingSoon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF4A67AD),
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text(
+          'Coming Soon',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
