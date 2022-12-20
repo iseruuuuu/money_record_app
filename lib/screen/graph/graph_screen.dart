14b99a6db8e4547d48c2c8728a79b7b808d3aa5a
iseruuuuu <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>
2022-12-20 18:19:16 +0900

    add: add obx in graph_screen.dart

--- lib/screen/graph/graph_screen.dart
+++ lib/screen/graph/graph_screen.dart
@@ -81,72 +81,75 @@ class GraphScreen extends StatelessWidget {
                 builder:
                     (BuildContext context, AsyncSnapshot<List<Todo>> snapshot) {
                   if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: controller.listLength.value,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Dismissible(
-                          key: Key(controller.amountIdList[index]),
-                          background: Container(
-                            alignment: Alignment.centerRight,
-                            color: Colors.red,
-                            child: const Padding(
-                              padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                              child: Icon(Icons.delete, color: Colors.white),
+                    return Obx(
+                      () => ListView.builder(
+                        itemCount: controller.listLength.value,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Dismissible(
+                            key: Key(controller.amountIdList[index]),
+                            background: Container(
+                              alignment: Alignment.centerRight,
+                              color: Colors.red,
+                              child: const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                                child: Icon(Icons.delete, color: Colors.white),
+                              ),
                             ),
-                          ),
-                          onDismissed: (direction) {
-                            bloc.delete(controller.amountIdList[index]);
-                            controller.loadInit();
-                          },
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  width: 1,
-                                  color: Colors.orange,
+                            onDismissed: (direction) {
+                              bloc.delete(controller.amountIdList[index]);
+                              controller.loadInit();
+                            },
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 1,
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                              ),
+                              child: ListTile(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DetailScreen(
+                                        id: controller.amountIdList[index],
+                                        discountPrice: controller
+                                            .amountDiscountPriceList[index],
+                                        buyPrice:
+                                            controller.amountBuyList[index],
+                                        category: controller
+                                            .amountCategoryList[index],
+                                        createdDate: controller
+                                            .amountCreatedTimeList[index],
+                                      ),
+                                    ),
+                                  );
+                                },
+                                title: Row(
+                                  children: [
+                                    Text(
+                                      controller.amountCategoryList[index],
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                trailing: Text(
+                                  "${controller.amountBuyList[index]}円",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
                             ),
-                            child: ListTile(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => DetailScreen(
-                                      id: controller.amountIdList[index],
-                                      discountPrice: controller
-                                          .amountDiscountPriceList[index],
-                                      buyPrice: controller.amountBuyList[index],
-                                      category:
-                                          controller.amountCategoryList[index],
-                                      createdDate: controller
-                                          .amountCreatedTimeList[index],
-                                    ),
-                                  ),
-                                );
-                              },
-                              title: Row(
-                                children: [
-                                  Text(
-                                    controller.amountCategoryList[index],
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              trailing: Text(
-                                "${controller.amountBuyList[index]}円",
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     );
                   }
                   return const Center(child: CircularProgressIndicator());
