81e79cb2e867f48d11fbfd3370d1549a3e333ea5
iseruuuuu <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>
2023-01-09 15:16:33 +0900

    add: add admob in home_screen.dart

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:money_records_app/screen/home/home_screen_controller.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +10,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final BannerAd myBanner = BannerAd(
+      adUnitId: Platform.isAndroid
+          //android
+          ? 'ca-app-pub-3940256099942544/6300978111'
+          //ios
+          : 'ca-app-pub-3471170179614589/7242925577',
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          ad.dispose();
+        },
+      ),
+    );
+
+    myBanner.load();
+    final AdWidget adWidget = AdWidget(ad: myBanner);
+    final Container adContainer = Container(
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width,
+      height: myBanner.size.height.toDouble(),
+      child: adWidget,
+    );
     final controller = Get.put(HomeScreenController());
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xFFFCF8EA),
       appBar: AppBar(
         backgroundColor: const Color(0xFF4A67AD),
@@ -24,85 +50,84 @@ class HomeScreen extends StatelessWidget {
       ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: SingleChildScrollView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 40, bottom: 2),
-                child: TextField(
-                  onChanged: (value) => controller.changeDiscountPrice(value),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: const InputDecoration(
-                    labelText: '節約できた値段',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40),
-                child: TextField(
-                  onChanged: (value) => controller.changeBuyPrice(value),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: const InputDecoration(
-                    labelText: '購入金額',
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-              TextField(
-                onChanged: (value) => controller.changeCategoryName(value),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 2),
+              child: TextField(
+                onChanged: (value) => controller.changeDiscountPrice(value),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 decoration: const InputDecoration(
-                  labelText: '種類（名前）',
+                  labelText: '節約できた値段',
                   filled: true,
                   fillColor: Colors.white,
                   border: InputBorder.none,
                 ),
               ),
-              Obx(
-                () => Padding(
-                  padding: const EdgeInsets.only(bottom: 42),
-                  child: ListTile(
-                    tileColor: Colors.white,
-                    leading: const Text('日付'),
-                    trailing: Text(controller.createdDate.value),
-                    onTap: () => controller.changeDateTime(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: TextField(
+                onChanged: (value) => controller.changeBuyPrice(value),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  labelText: '購入金額',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            TextField(
+              onChanged: (value) => controller.changeCategoryName(value),
+              decoration: const InputDecoration(
+                labelText: '種類（名前）',
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+              ),
+            ),
+            Obx(
+              () => Padding(
+                padding: const EdgeInsets.only(bottom: 42),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  leading: const Text('日付'),
+                  trailing: Text(controller.createdDate.value),
+                  onTap: () => controller.changeDateTime(),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF4A67AD),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () => controller.onTapStore(),
+                child: const Text(
+                  '保存',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                   ),
                 ),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 100,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color(0xFF4A67AD),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                  onPressed: () => controller.onTapStore(),
-                  child: const Text(
-                    '保存',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            const Spacer(),
+            adContainer,
+          ],
         ),
       ),
     );
