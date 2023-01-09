49d7cb91fafa5449a6e11027567157fc99f2f9a5
iseruuuuu <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>
2023-01-09 17:56:40 +0900

    add1.0: add dialog to check submit

--- lib/screen/home/home_screen_controller.dart
+++ lib/screen/home/home_screen_controller.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:money_records_app/database/db_bloc.dart';
 import 'package:money_records_app/model/money.dart';
+import 'package:panara_dialogs/panara_dialogs.dart';
 
 class HomeScreenController extends GetxController {
   RxBool isIncome = false.obs;
@@ -60,30 +61,39 @@ class HomeScreenController extends GetxController {
   }
 
   void onTapStore() {
-    final newMoney = Todo(
-      buyPrice: buyPrice.value,
-      discountPrice: discountPrice.value,
-      categoryName: categoryName.value,
-      createdDate: createdDates,
-    );
-    TodoBloc().create(newMoney);
-    openDialog();
+    //todo warningを消す。
+    if (buyPrice.value != '' &&
+        discountPrice.value != '' &&
+        categoryName.value != '') {
+      final newMoney = Todo(
+        buyPrice: buyPrice.value,
+        discountPrice: discountPrice.value,
+        categoryName: categoryName.value,
+        createdDate: createdDates,
+      );
+      TodoBloc().create(newMoney);
+      openDialog(isError: false);
+    } else {
+      openDialog(isError: true);
+    }
   }
 
-  //TODO ダイアログのデザインを修正する
-  void openDialog() {
-    Get.dialog(
-      AlertDialog(
-        title: const Center(child: Text('追加完了')),
-        actions: [
-          Center(
-            child: TextButton(
-              child: const Text("閉じる"),
-              onPressed: () => Get.back(),
-            ),
-          ),
-        ],
-      ),
+  void openDialog({required bool isError}) {
+    PanaraInfoDialog.show(
+      Get.context!,
+      title: isError ? "Error" : "Success",
+      message: isError
+          ? "One of them is empty.\n"
+              "Please check and Try again."
+          : "You can post contents.\n"
+              "please check it.",
+      buttonText: "OK",
+      panaraDialogType:
+          isError ? PanaraDialogType.error : PanaraDialogType.success,
+      barrierDismissible: false,
+      onTapDismiss: () {
+        Get.back();
+      },
     );
   }
 }
