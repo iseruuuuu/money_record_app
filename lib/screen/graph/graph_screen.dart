3d7e8371eda3d1bdb4fdb046f00ec3a516f1b14c
iseruuuuu <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>
2023-01-10 20:25:36 +0900

    fix1.0: resolved a bug in the first graph display and added colors

--- lib/screen/graph/graph_screen.dart
+++ lib/screen/graph/graph_screen.dart
@@ -69,134 +69,151 @@ class GraphScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: controller.amountCategoryList.isEmpty
-          ? const GraphEmptyScreen()
-          : Column(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  color: const Color(0xFFFCF8EA),
-                  height: 230,
-                  child: Obx(
-                    () => SfCircularChart(
-                      series: <CircularSeries>[
-                        PieSeries<ChartData, String>(
-                          dataLabelSettings:
-                              const DataLabelSettings(isVisible: true),
-                          dataSource: controller.chartData.value,
-                          pointColorMapper: (ChartData data, _) => data.color,
-                          xValueMapper: (ChartData data, _) => data.x,
-                          yValueMapper: (ChartData data, _) => data.y,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                const Divider(thickness: 3, color: Color(0xFF4A67AD)),
-                Obx(
-                  () => GraphListTile(
-                    leading: '合計',
-                    trailing: controller.amountBuyPrice.value.toString(),
-                  ),
-                ),
-                Obx(
-                  () => GraphListTile(
-                    leading: '節約できた金額',
-                    trailing: controller.amountSavePrice.value.toString(),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
+      body: Obx(
+        () => controller.amountCategoryList.isEmpty
+            ? const GraphEmptyScreen()
+            : Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
                     color: const Color(0xFFFCF8EA),
-                    //TODO 現在、StreamBuilderを使っていないため、取り除くor入れる
-                    child: StreamBuilder<List<Todo>>(
-                      stream: bloc.todoStream,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<Todo>> snapshot) {
-                        if (snapshot.hasData) {
-                          return Obx(
-                            () => ListView.builder(
-                              itemCount: controller.listLength.value,
-                              itemBuilder: (BuildContext context, int index) {
-                                return Dismissible(
-                                  key: Key(controller.amountIdList[index]),
-                                  background: Container(
-                                    alignment: Alignment.centerRight,
-                                    color: Colors.red,
-                                    child: const Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                                      child: Icon(Icons.delete,
-                                          color: Colors.white),
-                                    ),
-                                  ),
-                                  onDismissed: (direction) {
-                                    bloc.delete(controller.amountIdList[index]);
-                                    controller.loadInit();
-                                  },
-                                  child: Container(
-                                    decoration: const BoxDecoration(
-                                      border: Border(
-                                        bottom: BorderSide(
-                                          width: 1,
-                                          color: Colors.orange,
-                                        ),
-                                      ),
-                                    ),
-                                    child: ListTile(
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => DetailScreen(
-                                              id: controller
-                                                  .amountIdList[index],
-                                              discountPrice: controller
-                                                      .amountDiscountPriceList[
-                                                  index],
-                                              buyPrice: controller
-                                                  .amountBuyList[index],
-                                              category: controller
-                                                  .amountCategoryList[index],
-                                              createdDate: controller
-                                                  .amountCreatedTimeList[index],
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                      title: Row(
-                                        children: [
-                                          Text(
-                                            controller
-                                                .amountCategoryList[index],
-                                            style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      trailing: Text(
-                                        "${controller.amountBuyList[index]}円",
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          );
-                        }
-                        return const Center(child: CircularProgressIndicator());
-                      },
+                    height: 230,
+                    child: Obx(
+                      () => SfCircularChart(
+                        series: <CircularSeries>[
+                          PieSeries<ChartData, String>(
+                            dataLabelSettings:
+                                const DataLabelSettings(isVisible: false),
+                            dataSource: controller.chartData.value,
+                            pointColorMapper: (ChartData data, _) => data.color,
+                            xValueMapper: (ChartData data, _) => data.x,
+                            yValueMapper: (ChartData data, _) => data.y,
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                adContainer,
-              ],
-            ),
+                  const Divider(thickness: 3, color: Color(0xFF4A67AD)),
+                  Obx(
+                    () => GraphListTile(
+                      leading: '合計',
+                      trailing: controller.amountBuyPrice.value.toString(),
+                    ),
+                  ),
+                  Obx(
+                    () => GraphListTile(
+                      leading: '節約できた金額',
+                      trailing: controller.amountSavePrice.value.toString(),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: const Color(0xFFFCF8EA),
+                      //TODO 現在、StreamBuilderを使っていないため、取り除くor入れる
+                      child: StreamBuilder<List<Todo>>(
+                        stream: bloc.todoStream,
+                        builder: (BuildContext context,
+                            AsyncSnapshot<List<Todo>> snapshot) {
+                          if (snapshot.hasData) {
+                            return Obx(
+                              () => ListView.builder(
+                                itemCount: controller.listLength.value,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Dismissible(
+                                    key: Key(controller.amountIdList[index]),
+                                    background: Container(
+                                      alignment: Alignment.centerRight,
+                                      color: Colors.red,
+                                      child: const Padding(
+                                        padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                                        child: Icon(
+                                          Icons.delete,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    onDismissed: (direction) {
+                                      bloc.delete(controller.amountIdList[index]);
+                                      controller.loadInit();
+                                    },
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                        border: Border(
+                                          bottom: BorderSide(
+                                            width: 1,
+                                            color: Colors.orange,
+                                          ),
+                                        ),
+                                      ),
+                                      child: ListTile(
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => DetailScreen(
+                                                id: controller
+                                                    .amountIdList[index],
+                                                discountPrice: controller
+                                                        .amountDiscountPriceList[
+                                                    index],
+                                                buyPrice: controller
+                                                    .amountBuyList[index],
+                                                category: controller
+                                                    .amountCategoryList[index],
+                                                createdDate: controller
+                                                        .amountCreatedTimeList[index],
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        title: Row(
+                                          children: [
+                                            Text(
+                                              controller
+                                                  .amountCategoryList[index],
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        trailing: Wrap(
+                                          spacing: 20,
+                                          children: [
+                                            Container(
+                                              width: 30,
+                                              height: 30,
+                                              color: Color(controller
+                                                  .amountColorCodeList[index]),
+                                            ),
+                                            Text(
+                                              "${controller.amountBuyList[index]}円",
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            );
+                          }
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  adContainer,
+                ],
+              ),
+      ),
     );
   }
 }
