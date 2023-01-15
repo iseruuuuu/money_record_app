e62b4eccde3b4c7af7b454487de439b05b0a586e
iseruuuuu <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>
2023-01-15 10:50:49 +0900

    update2.0: update version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import in_app_review
 import package_info_plus_macos
 import path_provider_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
