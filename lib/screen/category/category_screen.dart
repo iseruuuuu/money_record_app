f4ea5d719be1ff0d32e44bd95a0628485afdbd1a
iseruuuuu <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>
2023-01-21 09:09:53 +0900

    add3.0: add obx and close keyboard

--- lib/screen/category/category_screen.dart
+++ lib/screen/category/category_screen.dart
@@ -20,79 +20,83 @@ class CategoryScreen extends StatelessWidget {
         elevation: 0,
         title: const Text('カテゴリー編集'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 10, right: 15, left: 15),
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              border: Border.all(width: 3),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search Text',
-                hintStyle: const TextStyle(fontSize: 18),
-                suffixIcon: IconButton(
-                  onPressed: controller.addList,
-                  icon: const Icon(Icons.add, size: 25),
-                ),
-                contentPadding: const EdgeInsets.only(top: 14, left: 10),
-                enabledBorder: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                isDense: true,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10, right: 15, left: 15),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                border: Border.all(width: 3),
+                borderRadius: BorderRadius.circular(10),
               ),
-              onChanged: controller.onChanged,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search Text',
+                  hintStyle: const TextStyle(fontSize: 18),
+                  suffixIcon: IconButton(
+                    onPressed: controller.addList,
+                    icon: const Icon(Icons.add, size: 25),
+                  ),
+                  contentPadding: const EdgeInsets.only(top: 14, left: 10),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  isDense: true,
+                ),
+                onChanged: controller.onChanged,
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: controller.categoryList.isNotEmpty
-                ? Obx(
-                    () => ListView.builder(
-                      itemCount: controller.categoryList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final categoryList = controller.categoryList;
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Card(
-                            child: ListTile(
-                              onTap: () => controller.onTapBack(index),
-                              contentPadding: EdgeInsets.zero,
-                              title: Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () =>
-                                        controller.deleteCategory(index),
-                                    icon: const Icon(
-                                      Icons.delete,
-                                      color: Colors.red,
-                                      size: 30,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width - 100,
-                                    child: Text(
-                                      categoryList[index],
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
+            const SizedBox(height: 10),
+            Obx(
+              () => Expanded(
+                child: controller.categoryList.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: controller.categoryList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final categoryList = controller.categoryList;
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Card(
+                              child: ListTile(
+                                onTap: () => controller.onTapBack(index),
+                                contentPadding: EdgeInsets.zero,
+                                title: Row(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () =>
+                                          controller.deleteCategory(index),
+                                      icon: const Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                        size: 30,
                                       ),
-                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                  ),
-                                ],
+                                    SizedBox(
+                                      width:
+                                          MediaQuery.of(context).size.width - 100,
+                                      child: Text(
+                                        categoryList[index],
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                : Container(),
-          )
-        ],
+                          );
+                        },
+                      )
+                    : Container(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
