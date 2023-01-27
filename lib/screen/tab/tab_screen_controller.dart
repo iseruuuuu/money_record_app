5daa228bf4f6f728969313914ba8aa8cfec60286
iseruuuuu <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>
2023-01-27 17:47:56 +0900

    fix6.0: remove new_version

--- lib/screen/tab/tab_screen_controller.dart
+++ lib/screen/tab/tab_screen_controller.dart
@@ -1,41 +1,13 @@
 // Package imports:
 import 'package:get/get.dart';
-import 'package:new_version/new_version.dart';
 
 // Project imports:
 import 'package:money_records_app/screen/calendar/calendar_screen_controller.dart';
 import 'package:money_records_app/screen/graph/graph_screen_controller.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 class TabScreenController extends GetxController
     with GetSingleTickerProviderStateMixin {
   RxInt selectedIndex = 0.obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-    checkUpdate();
-  }
-
-  void checkUpdate() async {
-    final newVersion = NewVersion(
-      iOSId: 'com.moneyRecordsApp',
-      iOSAppStoreCountry: 'JP',
-    );
-    final status = await newVersion.getVersionStatus();
-    final packageInfo = await PackageInfo.fromPlatform();
-    if (status != null && status.storeVersion != packageInfo.buildNumber) {
-      String storeVersion = status.storeVersion;
-      newVersion.showUpdateDialog(
-        context: Get.context!,
-        versionStatus: status,
-        dialogTitle: 'アップデートが必要です',
-        dialogText: 'Ver.$storeVersionが公開されています。\n最新バージョンのアップデートをお願いします。',
-        updateButtonText: 'アップデート',
-        dismissButtonText: 'あとで',
-      );
-    }
-  }
 
   void onTap(int index) {
     selectedIndex.value = index;
@@ -46,6 +18,7 @@ class TabScreenController extends GetxController
       final controller = Get.put(CalendarScreenController());
       final now = DateTime.now();
       controller.loadSelectDate(now);
+      controller.loadDate();
     }
   }
 }
