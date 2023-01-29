e642624582b98dd3c9448c6e78ba73535529aafb
iseruuuuu <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>
2023-01-29 18:59:16 +0900

    fix8.0: remove preference not use

--- lib/preference/shared_preference.dart
+++ lib/preference/shared_preference.dart
@@ -9,28 +9,6 @@ enum PreferenceKey {
 class Preference {
   final preference = SharedPreferences.getInstance();
 
-  Future<bool> getBool(PreferenceKey key) async {
-    final pref = await preference;
-    final value = pref.getBool(EnumToString.convertToString(key)) ?? false;
-    return value;
-  }
-
-  Future<void> setBool(PreferenceKey key, bool value) async {
-    final pref = await preference;
-    await pref.setBool(EnumToString.convertToString(key), value);
-  }
-
-  Future<int> getInt(PreferenceKey key) async {
-    final pref = await preference;
-    final value = pref.getInt(EnumToString.convertToString(key)) ?? 0;
-    return value;
-  }
-
-  Future<void> setInt(PreferenceKey key, int value) async {
-    final pref = await preference;
-    await pref.setInt(EnumToString.convertToString(key), value);
-  }
-
   Future<List<String>> getListString(PreferenceKey key) async {
     final pref = await preference;
     final value = pref.getStringList(EnumToString.convertToString(key)) ?? [];
