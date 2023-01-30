41ce8e8c0a37385d24280164a90e200e99bffd08
iseruuuuu <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>
2023-01-30 20:56:25 +0900

    fix8.0: add WidgetsFlutterBinding.ensureInitialized();

--- lib/database/db_provider.dart
+++ lib/database/db_provider.dart
@@ -1,4 +1,5 @@
 // Package imports:
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -19,6 +20,7 @@ class DBProvider {
   }
 
   Future<Database> initDB() async {
+    WidgetsFlutterBinding.ensureInitialized();
     final documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, 'moneyDB.db');
     return openDatabase(
